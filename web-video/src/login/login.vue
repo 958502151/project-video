43c2f827c2f455b6825dd3cba57465b1e05e7f00
ZhengYadong <zhengyadong@ztessc.com.cn>
2021-04-14 14:21:09 +0800

    登录页

--- web-video/src/login/login.vue
+++ web-video/src/login/login.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="login">
-        <img style="width: 400px;" src="../assets/login/jsx4.jpg"/>
-        <img src="../assets/login/jsx1.jpg"/>
-        <img src="../assets/login/jsx5.png"/>
-        <img src="../assets/login/jsx3.jpg"/>
-        <img src="../assets/login/jsx2.jpg"/>
-        <img src="../assets/login/jsx6.jpg"/>
-        <img style="width: 400px" src="../assets/login/jsx7.jpg"/>
+        <div class="login-box">
+            <span>用户名:<input/></span>
+            <br>
+            <span>密<span style="opacity: 0;">空</span>码:<input type="password"/></span>
+            <br>
+            <button type="submit">登录</button>
+            <button style="margin-left: 30%">注册</button>
+        </div>
+        <div class="backgroundVideo">
+            <video class="video" :width="videoW" muted="muted" loop="loop" autoplay="autoplay" src="../assets/login/videoXZSS.mp4"/>
+        </div>
     </div>
 </template>
 
@@ -15,13 +19,32 @@ import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
     setup() {
-        
+        // console.log(document.getElementsByTagName('body'));
+        // const videoW = document.getElementsByTagName('body')[0].clientWidth - 280;
+        return {
+            // videoW,
+        }
+        // document.getElementsByClassName('video')[0].clientWidth = videoW;
     },
 })
 </script>
 
 <style lang="scss">
     .login {
-        position: absolute;
+        width: 100%;
+        .backgroundVideo {
+            width: 100%;
+            .video {
+                width: 100%;
+            }
+        }
+        .login-box {
+            line-height: 40px;
+            margin: 20% 40%;
+            z-index: 999;
+            position: absolute;
+            float: left;
+            text-align: center;
+        }
     }
 </style>
--- web-video/src/main.js
+++ web-video/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+// import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')

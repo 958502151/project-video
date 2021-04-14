a9ffb66b7cdfc343e5c22fa5c1ef6c4bb4bf46e4
ZhengYadong <zhengyadong@ztessc.com.cn>
2021-04-14 22:47:12 +0800

    登录页更新，使用element-plus组件库

--- web-video/src/login/login.vue
+++ web-video/src/login/login.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="login">
         <div class="login-box">
-            <span>用户名:<input/></span>
+            <el-input size="small" v-model="username" placeholder="请输入用户名" prefix-icon="el-icon-user"/>
             <br>
-            <span>密<span style="opacity: 0;">空</span>码:<input type="password"/></span>
+            <el-input size="small" v-model="password" placeholder="请输入密码" suffix-icon="el-icon-view" prefix-icon="el-icon-warning" type="password" label="密码"/>
             <br>
-            <button type="submit">登录</button>
-            <button style="margin-left: 30%">注册</button>
+            <el-button plain size="mini" type="success">登录</el-button>
+            <el-button plain size="mini" style="margin-left: 30%">注册</el-button>
         </div>
         <div class="backgroundVideo">
             <video class="video" :width="videoW" muted="muted" loop="loop" autoplay="autoplay" src="../assets/login/videoXZSS.mp4"/>
@@ -15,14 +15,15 @@
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
     setup() {
-        // console.log(document.getElementsByTagName('body'));
-        // const videoW = document.getElementsByTagName('body')[0].clientWidth - 280;
+        console.log(document.getElementsByTagName('body'));
+        document.getElementsByTagName('body')[0].style.overflowY = 'hidden';
         return {
-            // videoW,
+            username: ref(''),
+            password: ref('')
         }
         // document.getElementsByClassName('video')[0].clientWidth = videoW;
     },
@@ -32,6 +33,7 @@ export default defineComponent({
 <style lang="scss">
     .login {
         width: 100%;
+        text-align: center;
         .backgroundVideo {
             width: 100%;
             .video {
@@ -39,12 +41,15 @@ export default defineComponent({
             }
         }
         .login-box {
-            line-height: 40px;
-            margin: 20% 40%;
+            line-height: 50px;
+            margin: 20% 45%;
             z-index: 999;
             position: absolute;
             float: left;
             text-align: center;
+            .el-input__inner {
+                width: 250px;
+            }
         }
     }
 </style>
--- web-video/src/main.js
+++ web-video/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-// import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus).mount('#app')

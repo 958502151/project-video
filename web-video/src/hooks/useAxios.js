08032e818f05c10663d6d7b46f1c77654f965c2c
ZhengYadong <zhengyadong@zookingsoft.com>
2021-04-27 17:07:50 +0800

    添加网络请求hooks

---
+++ web-video/src/hooks/useAxios.js
@@ -0,0 +1,25 @@
+// 封装网络请求方法
+
+import {ref} from 'vue'
+import axios from 'axios';
+function usURLAxios(url) {
+    const result = ref(null)
+    const loading = ref(true)
+    const loaded =ref(false)
+    const error =ref(null)
+    axios.get(url).then((res)=>{
+        loading.value = false
+        loaded.value = true
+        result.value = res.data
+    }).catch(e=>{
+        error.value = e
+        loading.value = false
+    })
+    return {
+        result, //响应成功结果
+        loading, //加载状态
+        loaded, //请求状态
+        error // 错误信息
+    }
+}
+export default usURLAxios

b8bdfdd98ffd62035d9c534755d824a3fead9f2a
ZhengYadong <zhengyadong@ztessc.com.cn>
2021-04-27 23:17:22 +0800

    导航条登录页不显示

--- web-video/src/App.vue
+++ web-video/src/App.vue
@@ -1,27 +1,33 @@
 <template>
   <div id="app">
-    <znavigation style="height: 10%"></znavigation>
+    <znavigation v-if="show" style="height: 10%"></znavigation>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import { defineComponent } from '@vue/composition-api'
 import znavigation from './components/znavigation'
+import { reactive, watch, ref } from 'vue'
+import { useRoute } from 'vue-router'
 
-export default {
+export default defineComponent({
   name: 'App',
   components: {
     znavigation,
   },
-  mounted() {
-    window.addEventListener('hashchange', () => {
-      const currentPath = window.location.href.slice(1);
-      if (this.$route.path !== currentPath) {
-        this.$router.push(currentPath);
-      }
-    }, false);
+  setup() {
+    const show = ref(reactive(false))
+    const route = useRoute()
+    watch(() => route.path,() => {
+      if (window.location.pathname !== '/login') show.value = true
+      else show.value = false
+    })
+    return {
+      show,
+    }
   },
-};
+})
 </script>
 
 <style>

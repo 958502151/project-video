40e0243ef1750fcf78c4e03da3daa692289d9333
ZhengYadong <zhengyadong@zookingsoft.com>
2021-06-08 18:23:48 +0800

    增加go文件项目

---
+++ web-serves/src/config/config.go
@@ -0,0 +1,10 @@
+package config
+
+import "fmt"
+
+func M() {
+	n := 1
+	m := 2
+	var c = m + n
+	fmt.Println(c)
+}
--- web-video/src/page/tentect/tentectVideo.vue
+++ web-video/src/page/tentect/tentectVideo.vue
@@ -1,14 +1,30 @@
 <template>
-    <iframe style="width: 100%; height: 90vh" src="https://v.qq.com/"/>
+    <iframe v-show="false" ref="iframe" style="width: 100%; height: 90vh" src="https://v.qq.com/"/>
+    <!-- <div v-html="'https://v.qq.com/'"></div> -->
+    <!-- <link href="https://v.qq.com/"/> -->
 </template>
 
 <script>
 
 import { defineComponent } from '@vue/composition-api'
+// import axios from 'axios';
+import { onMounted, ref } from 'vue'
 
 export default defineComponent({
     setup() {
-        
+        // axios.get('https://v.qq.com/').then(res => {
+        //     console.log(res)
+        //     debugger
+        // })
+        const iframe = ref('')
+        // document.getElementsByTagName
+        onMounted(() => {
+            console.log(iframe.value)
+        })
+
+        return {
+            iframe
+        }
     },
 })
 </script>
